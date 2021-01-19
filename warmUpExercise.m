174b61305419384d48857906ea749658e68ab42d
Abhineash Kishan <avikishan258@gmail.com>
2021-01-19 11:48:16 +0530

    Linear Regression with one variable to predict profit of food trucks

---
+++ warmUpExercise.m
@@ -0,0 +1,20 @@
+function A = warmUpExercise()
+%WARMUPEXERCISE
+%   A = WARMUPEXERCISE() is an example function that returns the 5x5 identity matrix
+
+A = [];
+% ============= YOUR CODE HERE ==============
+% Instructions: Return the 5x5 identity matrix 
+%                
+
+
+A=zeros(5);
+
+
+
+
+
+% ===========================================
+
+
+end
